d83064c06bb8908420df08b70637b78bb00c7a9e
saharlol <saharraveh970@gmail.com>
2026-02-04 15:29:37 +0200

    Update 1.9.2.js

--- scripts/1.9.2.js
+++ scripts/1.9.2.js
@@ -1,5 +1,5 @@
 /**
-** Custom Royale 1.9.2 **
+** Custom Royale 1.1 - 1.9.2 **
 ** https://github.com/shrlol/crstuff/ **
 ** Script by saharlol **
 */
@@ -10,52 +10,37 @@ const Libg = {
     }
 };
 
+console.log("Custom Royale 1.1 [1.9.2]\nRunning On:", Process.arch);
+
 const Config = {
-        // General
-        IP: "127.0.0.1",
+    Client:{
+        IP: "192.168.1.22",
         Port: 9339,
-        RC4Key: "fhsd6f86f67rt8fw78fw789we78r9789wer6re",
-        // Texts (TIDS)
+        RC4Key: "fhsd6f86f67rt8fw78fw789we78r9789wer6re"
+    },
+    Texts:{
         HelpButton: "Help",
         PrivacyButton: "Privacy Policy",
         ParentsButton: "Parents Guide",
         TosButton: "Terms Of Service",
         BattleTab: "Custom Royale",
         Settings: "Settings"
+    }
 }
 
-const Addresses = {
-    armv7: {
-        // Misc
+const Offsets = {
         CryptoKey: 0x45607F,
-        // Tids
+        NativeFontFormatString: 0x220D7C,
+        StringTableGetString: 0x5E708,
         TID_BUTTON_HELP: 0x43F267,
         TID_BUTTON_PRIVACY: 0x43F286,
         TID_BUTTON_PARENTS: 0x43F2C4,
         TID_BUTTON_TOS: 0x43F2A6,
         TID_TAB_BATTLE: 0x4420C1,
         TID_SETTINGS: 0x43108B,
-        // Battle Patches
         BATTLE_PATCH_1: 0x1C27F0,
         BATTLE_PATCH_2: 0x1C2B40
-    },
-    x86: {
-        // Misc
-        CryptoKey: 0x67BBD7,
-        // Tids
-        TID_BUTTON_HELP: 0x664DBF,
-        TID_BUTTON_PRIVACY: 0x664DDE,
-        TID_BUTTON_PARENTS: 0x664E1C,
-        TID_BUTTON_TOS: 0x664DFE,
-        TID_TAB_BATTLE: 0x667C19,
-        TID_SETTINGS: 0x656BE3,
-        // Battle Patches
-        BATTLE_PATCH_1: 0x27C140,
-        BATTLE_PATCH_2: 0x27C660
-    }
 };
-
-const Offsets = Process.arch === 'arm' ? Addresses.armv7 : Addresses.x86;
 
 const ntohs = new NativeFunction(Module.findExportByName('libc.so', 'ntohs'), 'uint16', ['uint16']);
 const inet_addr = new NativeFunction(Module.findExportByName('libc.so', 'inet_addr'), 'int', ['pointer']);
@@ -71,35 +56,44 @@ var saharlol = {
         unlock(Offsets.TID_SETTINGS);
         unlock(Offsets.CryptoKey);
         
-        Memory.writeUtf8String(Libg.offset(Offsets.TID_BUTTON_HELP), Config.HelpButton); 
-        Memory.writeUtf8String(Libg.offset(Offsets.TID_BUTTON_PRIVACY), Config.PrivacyButton); 
-        Memory.writeUtf8String(Libg.offset(Offsets.TID_BUTTON_PARENTS), Config.ParentsButton);
-        Memory.writeUtf8String(Libg.offset(Offsets.TID_BUTTON_TOS), Config.TosButton);
-        Memory.writeUtf8String(Libg.offset(Offsets.TID_TAB_BATTLE), Config.BattleTab);
-        Memory.writeUtf8String(Libg.offset(Offsets.TID_SETTINGS), Config.Settings);
-        Memory.writeUtf8String(Libg.offset(Offsets.CryptoKey), Config.RC4Key);
+        Memory.writeUtf8String(Libg.offset(Offsets.TID_BUTTON_HELP), Config.Texts.HelpButton); 
+        Memory.writeUtf8String(Libg.offset(Offsets.TID_BUTTON_PRIVACY), Config.Texts.PrivacyButton); 
+        Memory.writeUtf8String(Libg.offset(Offsets.TID_BUTTON_PARENTS), Config.Texts.ParentsButton);
+        Memory.writeUtf8String(Libg.offset(Offsets.TID_BUTTON_TOS), Config.Texts.TosButton);
+        Memory.writeUtf8String(Libg.offset(Offsets.TID_TAB_BATTLE), Config.Texts.BattleTab);
+        Memory.writeUtf8String(Libg.offset(Offsets.TID_SETTINGS), Config.Texts.Settings);
+        Memory.writeUtf8String(Libg.offset(Offsets.CryptoKey), Config.Client.RC4Key);
+        console.log("[*] Strings Replaced!")
         },
-        Battles: function(){
-        if (Process.arch === 'arm') {
-            // armv7
-            Memory.writeU8(Libg.offset(Offsets.BATTLE_PATCH_1), 0xD1); // D0 -> D1
-            Memory.writeU8(Libg.offset(Offsets.BATTLE_PATCH_2), 0x92); // 90 -> 92
-        } else {
-            // x86
-            Memory.writeU8(Libg.offset(Offsets.BATTLE_PATCH_1), 0xEB); // 74 -> EB
-            Memory.writeU8(Libg.offset(Offsets.BATTLE_PATCH_2), 0xC0); // 14 -> C0
-            Memory.writeU8(Libg.offset(0x2908C0), 0xEB); // 75 -> EB
+        Patches: function(){
+Interceptor.replace(base.add(Offsets.StringTableGetString), new NativeCallback(function(idPtr) {
+    const Native = new NativeFunction(base.add(Offsets.StringTableGetString), 'pointer', ['pointer'])
+    const tid = idPtr.readUtf8String()
+
+    if (tid === "INFO_BY_CLIENT_CONNECT") {
+        return Native(Memory.allocUtf8String("CRF | Loading...")) // No Limit
+    }
+
+    if (tid === "TID_ABOUT") {
+        return Native(Memory.allocUtf8String("pla")) // No Limit
+    }
+
+    if (tid === "TID_CREDITS") {
+        return Native(Memory.allocUtf8String("<cfce000>A<cfdc100>b<cffa200>o<cffa200>u<cff6c00>t</c>")) // No Limit
+    }
+    
+    return Native(idPtr)
+}, 'pointer', ['pointer']))
         }
-     }
     },
     core: {
       Setup: function() {
-        Interceptor.attach(Module.findExportByName(null, "getaddrinfo"), {
+        Interceptor.attach(Module.findExportByName('libc.so', 'connect'), {
             onEnter: function(args) {
-                if (args[0].readUtf8String() == "game.clashroyaleapp.com" || args[1].readUtf8String() == "9339") 
-                {
-                    args[0].writeUtf8String(Config.IP);
-                    args[1].writeUtf8String(Config.Port);
+                if (ntohs(Memory.readU16(args[1].add(2))) === 9339) {
+                    Memory.writeU16(args[1].add(2), ntohs(Config.Client.Port));
+                    Memory.writeInt(args[1].add(4), inet_addr(Memory.allocUtf8String(Config.Client.IP)));
+                    console.log("[*] IP Redirected")
                 }
             }
         });
@@ -117,8 +111,8 @@ function unlock(addr) {
 rpc.exports = {
     init: function(stage, options) {
         Interceptor.detachAll();
+        //saharlol.misc.Patches();
         saharlol.misc.Strings();
-        saharlol.misc.Battles();
         saharlol.core.Setup();
     }
 };
